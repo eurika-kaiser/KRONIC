f2942191ca2d6b24a5f6e3459d75d550d52804d5
Eurika Kaiser <eurika.kaiser@gmail.com>
2018-04-10 18:08:17 -0700

    Adapted cost function for fair comparison

--- SlowManifold.m
+++ SlowManifold.m
@@ -1,5 +1,6 @@
 clear all , close all , clc 
 path2figs = './../Figures/SLOW_MANIFOLD/'; mkdir(path2figs)
+addpath('./utils');
 
 %% System
 mu          = -0.1;
@@ -55,6 +56,7 @@ yIC(2,:,:) = y0;
 mu          = -.1;
 lambda      = 1;
 ModelName   = 'SlowManifold_B01_';
+ModelName   = [path2figs,ModelName];
 B = [0; 1];
 A = [mu 0; 0 lambda]; 
 R = 1;
@@ -90,7 +92,7 @@ SlowManifold_PhasePlot_Control
 clear all , close all , clc
 path2figs = './../Figures/SLOW_MANIFOLD/';
 dt = 0.01;
-tspan       = 0:dt:50; %50 
+tspan       = 0:dt:50; 
 x0          = [-5; 5];
 subindex    = @(A,r,c) A(r,c);      %# An anonymous function to index a matrix
 
@@ -102,31 +104,34 @@ lambda      = 1;
 b           = lambda/(lambda-2*mu);
 
 % LQR on linearized system
-A = [mu 0; 0 lambda]; 
-Q = eye(2);
-R = 1;
-C = lqr(A,B,Q,R);
-vf = @(t,x) A*x + [0; -lambda*x(1)^2] - B*C*x; 
-[t,xLQR] = ode45(vf,tspan,x0);
-uLQR = (C*xLQR')';
-JLQR_x   = cumsum(xLQR(:,1).^2 + xLQR(:,2).^2 + uLQR.^2)'; 
-JLQR_phi = cumsum(xLQR(:,1).^2 + xLQR(:,2).^2 + uLQR.^2 + ... % JLQR_x part
-                 (xLQR(:,2)-b*xLQR(:,1).^2).^2); % via phi
+A = [mu 0; 0 lambda];   % system matrix from linearization
+Q = eye(2);             % state penalization
+R = 1;                  % control penalization
+C = lqr(A,B,Q,R);       % gain
+vf = @(t,x) A*x + [0; -lambda*x(1)^2] - B*C*x; % apply gain in real system
+[t,xLQR] = ode45(vf,tspan,x0); 
+uLQR = (C*xLQR')';      % recalculate control input
+JLQR_x   = cumsum(xLQR(:,1).^2 + xLQR(:,2).^2 + uLQR.^2)'; % standard LQR cost
+% JLQR_phi = cumsum(xLQR(:,1).^2 + xLQR(:,2).^2 + uLQR.^2 + ... % cost in eigenfun, JLQR_x part
+%                  (xLQR(:,2)-b*xLQR(:,1).^2).^2); % via phi
+JLQR_phi = cumsum(xLQR(:,1).^2 + xLQR(:,1).^2 + uLQR.^2 + ... % cost in eigenfun,
+                 (xLQR(:,2)-b*xLQR(:,1).^2).^2); % via phi             
 
+             
 % Koopman operator optimal control (KOOC); i.e., LQR on Koopman operator
-A2 = [mu 0 0; 0 lambda -lambda; 0 0 2*mu]; 
-B2 = [B; 0];
-Q2 = [1 0 0; 0 1 0; 0 0 0];
+A2 = [mu 0 0; 0 lambda -lambda; 0 0 2*mu]; % system matrix in (nonlinear) observables
+B2 = [B; 0];                               % new control vector in observables
+Q2 = [1 0 0; 0 1 0; 0 0 0];                % weight matrix only penalizing x1,x2
 R = 1;
-C2 = lqr(A2,B2,Q2,R); % CONSTANT
-% note that controller is nonlinear in the state x1
+C2 = lqr(A2,B2,Q2,R); 
+% note that controller becomes nonlinear in the state x1
 vf2 = @(t,x) A*x + [0; -lambda*x(1)^2] - B*(C2(1:2)*x + C2(3)*x(1)^2);
 [t,xKOOC] = ode45(vf2,tspan,x0);
 uKOOC = (C2(1:2)*xKOOC')'+C2(3)*xKOOC(:,1).^2;
 JKOOC_x = cumsum(xKOOC(:,1).^2 + xKOOC(:,2).^2 + uKOOC.^2)'; 
-JKOOC_phi = cumsum( xKOOC(:,1).^2 + xKOOC(:,2).^2 + uKOOC.^2 + ... % J_x part
+JKOOC_phi = cumsum( xKOOC(:,1).^2 + (xKOOC(:,1).^2).^2 + uKOOC.^2 + ... % J_x part
                    (xKOOC(:,2)-b*xKOOC(:,1).^2).^2); % via phi
-
+               
 % Check
 [Vy,Dy] = eig(A2');
 Vy^(-1)*A2'*Vy
@@ -152,9 +157,8 @@ vf3 = @(t,x) A*x + [0; -lambda*x(1)^2] - B*(Cphi(1:2)*x + Cphi(3)*x(1)^2);
 [t,xKOOC2] = ode45(vf3,tspan,x0);
 uKOOC2 = (Cphi(1:2)*xKOOC2')' + Cphi(3)*xKOOC2(:,1).^2;
 JKOOC2_x = cumsum(xKOOC2(:,1).^2 + xKOOC2(:,2).^2 + uKOOC2.^2 )';
-JKOOC2_phi = cumsum(xKOOC2(:,1).^2 + xKOOC2(:,2).^2 + uKOOC2.^2 + ... % J_x part
-                   (xKOOC2(:,2)-b*xKOOC2(:,1).^2).^2); % via phi
-           
+JKOOC2_phi = cumsum(xKOOC2(:,1).^2 + (xKOOC2(:,1).^2).^2 + uKOOC2.^2 + ... % J_x part
+                   (xKOOC2(:,2)-b*xKOOC2(:,1).^2).^2); % via phi           
              
 % "Analytic"
 N = size(Aphi,1);
@@ -178,9 +182,9 @@ xNLC = sol.y(1:2,:)';
 tNLC = sol.x;
 uNLC = R^(-1)*B'*sol.y(3:4,:); uNLC = uNLC';
 JNLC_x      = cumsum(xNLC(:,1).^2 + xNLC(:,2).^2 + (uNLC).^2)'; 
-JNLC_phi    = cumsum(xNLC(:,1).^2 + xNLC(:,2).^2 + (uNLC).^2 + ... % J_x part
+JNLC_phi    = cumsum(xNLC(:,1).^2 + (xNLC(:,1).^2).^2 + (uNLC).^2 + ... % J_x part
                      (xNLC(:,2)-b*xNLC(:,1).^2).^2);               % via phi
- 
+                 
 %A*sol.y(1:2,1) + [0; -lambda*sol.y(1,1)^2] - B*R^(-1)*B'*sol.y(3:4,1)
 
 
@@ -193,27 +197,35 @@ vf5 = @(t,x) A*x + [0; -lambda*x(1)^2] - B*(-lambda*x(1)^2 + C5*x);
 [t,xFL] = ode45(vf5,tspan,x0);
 uFL = (-lambda*(xFL(:,1).^2)' + C5*xFL')';
 JFL_x   = cumsum(xFL(:,1).^2 + xFL(:,2).^2 + uFL.^2)'; 
-JFL_phi = cumsum(xFL(:,1).^2 + xFL(:,2).^2 + uFL.^2 + ... % JLQR_x part
+JFL_phi = cumsum(xFL(:,1).^2 + (xFL(:,1).^2).^2 + uFL.^2 + ... % JLQR_x part
                 (xFL(:,2)-b*xFL(:,1).^2).^2);               % via phi
 
-% Plot (FIGURE 5)
+% Plot figures
 showThree = 1;
 ylim_vals = [-15 10];
 axis_lim = [0 50 0 600000];
-% axis_lim = [0 50 0 100000000];
 showResults_SlowManifold
 
+
 %% Balanced model
-sys_y = ss(A2,B2,eye(3),0);
-tf_y = tf(sys_y);
-[sys_y_bal,g] = balreal(sys_y);
-rsys = balred(sys_y,2);
+% sys_y = ss(A2,B2,eye(3),0);
+% tf_y = tf(sys_y);
+% [sys_y_bal,g] = balreal(sys_y);
+% rsys = balred(sys_y,2);
+% 
+% sys_phi = ss(Aphi,Bphi,eye(3),0);
+% [sys_phi_bal,g_phi] = balreal(sys_phi);
+% rsys_phi = balred(sys_phi,2);
+% 
+% [sysr,u] = minreal(sys_phi);
 
-sys_phi = ss(Aphi,Bphi,eye(3),0);
-[sys_phi_bal,g_phi] = balreal(sys_phi);
-rsys_phi = balred(sys_phi,2);
+% LQR on minreal
+% Q = eye(3);
+% C = lqr(u*sys_phi.A*u',u*sys_phi.B,Q,R);       % gain
+% vf = @(t,x) A*x + [0; -lambda*x(1)^2] - B*C*x; % apply gain in real system
+% [t,xKD] = ode45(vf,tspan,x0); 
 
-%% Cost function Q = eye (FIGURE 6)
+%% Cost function Q = eye 
 % Koopman operator optimal control (KOOC)
 A2 = [mu 0 0; 0 lambda -lambda; 0 0 2*mu]; 
 B2 = [B; 0];
@@ -224,9 +236,9 @@ vf2 = @(t,x) A*x + [0; -lambda*x(1)^2] - B*(C2(1:2)*x + C2(3)*x(1)^2);
 [t,xKOOC] = ode45(vf2,tspan,x0);
 uKOOC = (C2(1:2)*xKOOC')'+C2(3)*xKOOC(:,1).^2;
 JKOOC_x = cumsum(xKOOC(:,1).^2 + xKOOC(:,2).^2 + uKOOC.^2)'; 
-JKOOC_phi = cumsum( xKOOC(:,1).^2 + xKOOC(:,2).^2 + uKOOC.^2 + ... % J_x part
-                 -2*b*xKOOC(:,1).^2.*xKOOC(:,2) + b^2*xKOOC(:,1).^4)'; % via phi
-                 
+JKOOC_phi = cumsum( xKOOC(:,1).^2 + (xKOOC(:,1).^2).^2 + uKOOC.^2 + ... % J_x part
+                    (xKOOC(:,2)-b*xKOOC(:,1).^2).^2); % via phi
+                
 % KC with eigenfunctions
 b = lambda/(lambda-2*mu);
 T = [1 0 0; 0 1 -b; 0 0 1]; % Transformation between y and phi
@@ -242,9 +254,8 @@ vf3 = @(t,x) A*x + [0; -lambda*x(1)^2] - B*(Cphi(1:2)*x + Cphi(3)*x(1)^2);
 [t,xKOOC2] = ode45(vf3,tspan,x0);
 uKOOC2 = (Cphi(1:2)*xKOOC2')' + Cphi(3)*xKOOC2(:,1).^2;
 JKOOC2_x = cumsum(xKOOC2(:,1).^2 + xKOOC2(:,2).^2 + uKOOC2.^2 )';
-JKOOC2_phi = cumsum(xKOOC2(:,1).^2 + xKOOC2(:,2).^2 + uKOOC2.^2 + ... % J_x part
-                 - 2*b*xKOOC2(:,1).^2.*xKOOC2(:,2) + b^2*xKOOC2(:,1).^4)'; % via phi
-
+JKOOC2_phi = cumsum(xKOOC2(:,1).^2 + (xKOOC2(:,1).^2).^2 + uKOOC2.^2 + ... % J_x part
+                    (xKOOC2(:,2)-b*xKOOC2(:,1).^2).^2); % via phi
 
 % Show comparison
 figure,hold on, box on
@@ -274,87 +285,135 @@ print('-painters','-depsc2', '-loose', [ModelName,'Cost_Jphi','.eps']);
 %% Control
 clear all , close all , clc
 dt = 0.01;
-tspan = 0:dt:50; 
+tspan = 0:dt:50;%50; 
 x0 = [-5; 5];
 subindex = @(A,r,c) A(r,c);      %# An anonymous function to index a matrix
 
 path2figs = './../Figures/SLOW_MANIFOLD/';
 %% OPTIMAL CONTROL B = [1; 0]
-axis_lim = [0 50 0 15000];
+axis_lim = [0 50 0 20000];
 
 ModelName = [path2figs,'SlowManifold_B10_'];
 B       = [1; 0];
 mu      = 0.1;
 lambda  = -1;
 b       = lambda/(lambda-2*mu);
+R       = 1;
+RKRONIC = 4.0;
+phifun3 = @(x) [x(1); x(2)-b*x(1)^2; x(1)^2];
 
 % LQR on linearized system
-A = [mu 0; 0 lambda]; 
+A = [mu 0; 0 lambda];
 Q = eye(2);
-R = 1;
-try
-    C = lqr(A,B,Q,R);
-    vf = @(t,x) A*x + [0; -lambda*x(1)^2] - B*C*x; 
-    [t,xLQR] = ode45(vf,tspan,x0);
-catch
-    warning('Linearized system not stabilizable.')
-    xLQR = zeros(length(tspan),2);
-end
+C = lqr(A,B,Q,R);
+vf = @(t,x) A*x + [0; -lambda*x(1)^2] - B*C*x;
+[t,xLQR] = ode45(vf,tspan,x0);
 uLQR = (C*xLQR')';
-JLQR_x   = cumsum(xLQR(:,1).^2 + xLQR(:,2).^2 + uLQR.^2)'; 
-JLQR_phi = cumsum(xLQR(:,1).^2 + xLQR(:,2).^2 + uLQR.^2 ... % JLQR_x part
-            - 2*b*xLQR(:,1).^2.*xLQR(:,2) + b^2*xLQR(:,1).^4)';  
-        
+phiLQR = zeros(length(t),3);
+for i = 1:length(t)
+    phiLQR(i,:) = phifun3(xLQR(i,:));
+end
+JLQR_x   = cumsum(xLQR(:,1).^2 + xLQR(:,2).^2 + R*uLQR.^2)';
+JLQR_phi = cumsum(phiLQR(:,1).^2 + phiLQR(:,2).^2 + RKRONIC*uLQR.^2);
+              
 % Control with truncated Koopman system; i.e., LQR on Koopman "matrix" (fails)
-A2 = [mu 0 0; 0 lambda -lambda; 0 0 2*mu]; 
+A2 = [mu 0 0; 0 lambda -lambda; 0 0 2*mu];
 Q2 = [1 0 0; 0 1 0; 0 0 0];
-R = 1;
+
+% Minreal
+% B2 = [B; 2*x0(1)];
+% sys_y = ss(A2,B2,eye(3),0);
+% [sys_balreal,g,T,Ti] = balreal(sys_y);
+% sys_balred = balred(sys_y,2);
+% [sys_minreal,u_mr] = minreal(sys_y);
+% tf_y = tf(sys_y);
+
 try
-    C2 = @(x) lqr(A2,[B; 2*x(1)],Q2,R); 
+    C2 = @(x) lqr(A2,[B; 2*x(1)],Q2,0.01*R);
     vf2 = @(t,x) A*x + [0; -lambda*x(1)^2] - B*(subindex(C2(x),1,1:2)*x + subindex(C2(x),1,3)*x(1)^2);
     [t,xKOOC] = ode45(vf2,tspan,x0); % fails at some point
+    
+%         B2 = @(x) [[B; 2*x(1)], [0;1;0]];
+%         C2 = @(x) lqr(A2,B2(x),Q2,0.05*eye(2));
+%         vf2 = @(t,x) A*x + [0; -lambda*x(1)^2] - B*(subindex(C2(x),1,1:2)*x + subindex(C2(x),1,3)*x(1)^2);
+%         [t,xKOOC] = ode45(vf2,tspan,x0); % fails at some point
+%         
+%         B2 = @(x) [[B; 2*x(1)], [0;1;0]];
+%         C2 = @(x) lqr(A2,B2(x),Q2,eye(2)); % getBRSys(sys_y,B2(x)),R,
+%         T  = @(x) getBRTrafo(sys_y,B2(x));
+%         vf2 = @(t,x) A*x + [0; -lambda*x(1)^2] - B*C2(x)*T(x)*[x;x(1).^2];
+%         [t,xKOOC] = ode45(vf2,tspan,x0); % fails at some point
+    uKOOC = zeros(length(t),1);
+    for i = 1:length(t)
+        uKOOC(i) = subindex(C2(xKOOC(i,:)),1,1:2)*xKOOC(i,:)' + subindex(C2(xKOOC(i,:)),1,3)*xKOOC(i,1)^2;
+    end
 catch
     warning('Truncated Koopman system not stabilizable.')
     xKOOC = zeros(length(tspan),2);
     C2 = zeros(1,3);
+    uKOOC = (C2(1:2)*xKOOC')'+C2(3)*xKOOC(:,1).^2; 
 end
-uKOOC = (C2(1:2)*xKOOC')'+C2(3)*xKOOC(:,1).^2;
-JKOOC_x = cumsum(xKOOC(:,1).^2 + xKOOC(:,2).^2 + uKOOC.^2)'; 
-JKOOC_phi = cumsum( xKOOC(:,1).^2 + xKOOC(:,2).^2 + uKOOC.^2 + ... % J_x part
-                 -2*b*xKOOC(:,1).^2.*xKOOC(:,2) + b^2*xKOOC(:,1).^4)'; % via phi
-             
+
+phiKOOC = zeros(length(t),3);
+for i = 1:length(t)
+    phiKOOC(i,:) = phifun3(xKOOC(i,:));
+end
+JKOOC_x = cumsum(xKOOC(:,1).^2 + xKOOC(:,2).^2 + R*uKOOC.^2)';
+JKOOC_phi = cumsum(phiKOOC(:,1).^2 + phiKOOC(:,2).^2 + RKRONIC*uKOOC.^2);
+
 % KRONIC 
+% Truncated 2D
 b = lambda/(lambda-2*mu);
-T = [1 0 0; 0 1 -b];
+T = [1 0 0; 0 1 -b]; % y -> phi
 Aphi = [mu 0; 0 lambda];
-gradphi = @(x) ([1 0; -2*b*x(1) 1]); 
-gain = @(x)(lqr(Aphi,(gradphi(x)*B),Q,R)*T);
+gradphi = @(x) ([1 0; -2*b*x(1) 1]);
+Qphi = eye(2);
+gain = @(x)(lqr(Aphi,(gradphi(x)*B),Qphi,RKRONIC)*T);
 vf3 = @(t,x) A*x + [0; -lambda*x(1)^2] - B*(subindex(gain(x),1,1:2)*x + subindex(gain(x),1,3)*x(1)^2);
+% gain = @(x)(lqr(Aphi,(gradphi(x)*B),Qphi,R));
+% vf3 = @(t,x) A*x + [0; -lambda*x(1)^2] - B*gain(x)*phifun2(x); % above are identical to this
 [t,xKOOC2] = ode45(vf3,tspan,x0);
 
+% 3D
+% b = lambda/(lambda-2*mu);
+% T = [1 0 0; 0 1 -b; 0, 0, 1]; 
+% Aphi = [mu 0 0; 0 lambda 0; 0 0 2*mu];
+% gradphi = @(x) ([1 0; -2*b*x(1) 1; 2*x(1) 0]);
+% % Qphi = eye(3);
+% Qphi = [1 0 0; 0 1 b; 0 b b^2];
+% gain = @(x)(lqr(Aphi,(gradphi(x)*B),Qphi,0.01*RKRONIC)*T); %RKRONIC
+% vf3 = @(t,x) A*x + [0; -lambda*x(1)^2] - B*(subindex(gain(x),1,1:2)*x + subindex(gain(x),1,3)*x(1)^2);
+% [t,xKOOC2] = ode45(vf3,tspan(1:10),x0);
+
+
 uKOOC2 = zeros(length(t),1);
+phiKOOC2 = zeros(length(t),3);
 for i = 1:length(t)
     uKOOC2(i) = subindex(gain(xKOOC2(i,:)),1,1:2)*xKOOC2(i,:)' + subindex(gain(xKOOC2(i,:)),1,3)*xKOOC2(i,1)^2;
+%     uKOOC2(i) = gain(xKOOC2(i,:))*phifun2(xKOOC2(i,:));
+    phiKOOC2(i,:) = phifun3(xKOOC2(i,:));
 end
-JKOOC2_x = cumsum(xKOOC2(:,1).^2 + xKOOC2(:,2).^2 + uKOOC2.^2 )';
-JKOOC2_phi = cumsum(xKOOC2(:,1).^2 + xKOOC2(:,2).^2 + uKOOC2.^2 + ... % J_x part
-                 - 2*b*xKOOC2(:,1).^2.*xKOOC2(:,2) + b^2*xKOOC2(:,1).^4)'; % via phi
-             
+JKOOC2_x = cumsum(xKOOC2(:,1).^2 + xKOOC2(:,2).^2 + R*uKOOC2.^2 )';
+JKOOC2_phi = cumsum(phiKOOC2(:,1).^2 + phiKOOC2(:,2).^2 + RKRONIC*uKOOC2.^2);
 
+                
 % Nonlinear Control
 vf4 = @(t,x) [A*x(1:2) + [0; -lambda*x(1)^2] - B*R^(-1)*B'*x(3:4); ...
-              -Q*x(1:2) - A'*x(3:4) - [0; lambda*x(1)]];
+    -Q*x(1:2) - A'*x(3:4) - [0; lambda*x(1)]];
 vf4_bc = @(xa,xb) [xa(1:2)-x0; xb(3:4)-eye(2)*xb(1:2)];
-vf4_init = @(t) [x0; 0; 0]; 
+vf4_init = @(t) [x0; 0; 0];
 solinit = bvpinit(tspan, vf4_init);
 sol     = bvp4c(vf4,vf4_bc,solinit);
 xNLC = sol.y(1:2,:)';
 tNLC = sol.x;
 uNLC = R^(-1)*B'*sol.y(3:4,:); uNLC = uNLC';
-JNLC_x = cumsum(xNLC(:,1).^2 + xNLC(:,2).^2 + (uNLC).^2)'; 
-JNLC_phi = cumsum( xNLC(:,1).^2 + xNLC(:,2).^2 + (uNLC).^2 + ... % J_x part
-                 -2*b*xNLC(:,1).^2.*xNLC(:,2) + b^2*xNLC(:,1).^4)'; % via phi
-
+phiNLC = zeros(length(t),3);
+for i = 1:length(t)
+    phiNLC(i,:) = phifun3(xNLC(i,:));
+end
+JNLC_x = cumsum(xNLC(:,1).^2 + xNLC(:,2).^2 + R*(uNLC).^2)';
+JNLC_phi = cumsum(phiNLC(:,1).^2 + phiNLC(:,2).^2 + RKRONIC*uNLC.^2);
+              
 % Feedback linearization (fails, set to zero)
 % A5 = [0 1; -2*mu*lambda lambda+2*mu];
 % B5 = [0;1];
@@ -363,14 +422,108 @@ JNLC_phi = cumsum( xNLC(:,1).^2 + xNLC(:,2).^2 + (uNLC).^2 + ... % J_x part
 % C5 = lqr(A5,B5,Q5,R5);
 % vf5 = @(t,x) A*x + [0; -lambda*x(1)^2] + B*(1/(2*lambda*x(1))*C5*x); 
 % [t,xFL] = ode45(vf5,tspan,x0);             
-xFL = zeros(length(tspan),2);
+xFL = zeros(length(tspan),2); % feedback linear. doesn't work, set to zero for plotting
 C5 = zeros(1,2);
 uFL = ((1./(2*lambda*xFL(:,1)')).*(C5*xFL'))';
-JFL_x   = cumsum(xFL(:,1).^2 + xFL(:,2).^2 + uFL.^2)'; 
-JFL_phi = cumsum(xFL(:,1).^2 + xFL(:,2).^2 + uFL.^2 ... % JLQR_x part
-            - 2*b*xFL(:,1).^2.*xFL(:,2) + b^2*xFL(:,1).^4)';  
-
+JFL_x   = cumsum(xFL(:,1).^2 + xFL(:,2).^2 + R*uFL.^2)'; 
+phiFL = zeros(length(t),3);
+for i = 1:length(t)
+    phiFL(i,:) = phifun3(phiFL(i,:));
+end
+JFL_phi = cumsum(phiFL(:,1).^2 + phiFL(:,2).^2 + RKRONIC*uFL.^2);
+            
 ylim_vals = [-5 10];
 showThree = 1;
 showResults_SlowManifold
+
+return
+%% Different plots
+figure, box on
+plot(xNLC(:,1),xNLC(:,2),'-k','LineWidth',2), hold on
+plot(xLQR(:,1),xLQR(:,2),'--r','LineWidth',2)
+% plot(xKOOC(:,1),xKOOC(:,2),'-.m','LineWidth',2)
+plot(xKOOC2(:,1),xKOOC2(:,2),':b','LineWidth',2)
+title('Phase Plot')
+
+figure, box on
+plot(xNLC(:,1),'-k','LineWidth',2), hold on
+plot(xNLC(:,2),'-k','LineWidth',2), 
+plot(xLQR(:,1),'--r','LineWidth',2)
+plot(xLQR(:,2),'--r','LineWidth',2)
+plot(xKOOC2(:,1),':b','LineWidth',2)
+plot(xKOOC2(:,2),':b','LineWidth',2)
+% plot(xKOOC(:,1),'-.m','LineWidth',2)
+% plot(xKOOC(:,2),'-.m','LineWidth',2)
+title('Time series')
+
+figure, box on
+semilogy((JNLC_x),'-k','LineWidth',2), hold on
+semilogy((JLQR_x),'--r','LineWidth',2)
+semilogy((JKOOC2_x),':b','LineWidth',2)
+semilogy((JKOOC_x),'-.m','LineWidth',2)
+title('Jx')
+
+figure, box on
+semilogy((JNLC_phi),'-k','LineWidth',2), hold on
+semilogy((JLQR_phi),'--r','LineWidth',2)
+semilogy((JKOOC2_phi),':b','LineWidth',2)
+semilogy((JKOOC_phi),'-.m','LineWidth',2)
+title('Jphi')
+
+figure, hold on, box on
+plot(cumsum((uNLC).^2),'-k','LineWidth',2)
+plot(cumsum((uLQR).^2),'--r','LineWidth',2)
+plot(cumsum((uKOOC2).^2),':b','LineWidth',2)
+plot(cumsum((uKOOC).^2),'-.m','LineWidth',2)
+title('sum u^2')
+
+% figure, hold on, box on
+% plot(cumsum((uNLC).^2),'-k','LineWidth',2)
+% plot(cumsum((uLQR).^2),'--r','LineWidth',2)
+% plot(cumsum((uKOOC2).^2),':b','LineWidth',2)
+%%
+figure,
+subplot(3,1,1)
+hold on, box on, title('LQR')
+plot(cumsum((uLQR).^2),'-k','LineWidth',2)
+plot(cumsum(xLQR(:,1).^2),'--r','LineWidth',2)
+plot(cumsum((xLQR(:,2)-b*xLQR(:,1).^2).^2),':b','LineWidth',2)
+plot(JLQR_phi,'-g','LineWidth',2)
+ylim([0 15000])
+subplot(3,1,2)
+hold on, box on, title('KRONIC')
+plot(cumsum(RKRONIC*(uKOOC2).^2),'-k','LineWidth',2)
+plot(cumsum(xKOOC2(:,1).^2),'--r','LineWidth',2)
+plot(cumsum((xKOOC2(:,2)-b*xKOOC2(:,1).^2).^2),':b','LineWidth',2)
+plot(JKOOC2_phi,'-g','LineWidth',2)
+ylim([0 15000])
+subplot(3,1,3)
+hold on, box on, title('NLC')
+plot(cumsum((uNLC).^2),'-k','LineWidth',2)
+plot(cumsum(xNLC(:,1).^2),'--r','LineWidth',2)
+plot(cumsum((xNLC(:,2)-b*xNLC(:,1).^2).^2),':b','LineWidth',2)
+plot(JNLC_phi,'-g','LineWidth',2)
+legend('u','x1','x2-bx1^2','J')
+ylim([0 15000])
+
+%%
+xspace = [-5:0.1:5]; Nx = length(xspace);
+[X,Y] = meshgrid(xspace,xspace);
+PHI = zeros(Nx,Nx,3);
+for ix = 1:Nx
+    for iy = 1:Nx
+        PHI(iy,ix,1:3) = phifun3([X(iy,ix); Y(iy,ix)]);
+    end
+end
+figure,
+subplot(1,3,1)
+imagesc(xspace,xspace,PHI(:,:,1))
+title('phi1')
+subplot(1,3,2)
+imagesc(xspace,xspace,PHI(:,:,2))
+title('phi2')
+subplot(1,3,3)
+imagesc(xspace,xspace,PHI(:,:,3))
+title('phi3')
+
 
